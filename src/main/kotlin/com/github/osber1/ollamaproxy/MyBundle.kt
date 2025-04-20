f196d73f005cc2b19c8db13c723526c3d180ea2e
GitHub Action <action@github.com>
2025-04-20 20:53:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/osber1/ollamaproxy/MyBundle.kt
+++ src/main/kotlin/com/github/osber1/ollamaproxy/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.osber1.ollamaproxy
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/osber1/ollamaproxy/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/osber1/ollamaproxy/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.osber1.ollamaproxy.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
